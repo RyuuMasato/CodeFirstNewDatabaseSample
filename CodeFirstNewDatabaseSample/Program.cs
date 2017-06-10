dba85c1d63cfb64c8090c130b15e1319e0e310b6
Jorrit Meulenbeld <jorrit.meulenbeld@outlook.com>
2017-06-10 12:26:29 +0200

    init commit

--- CodeFirstNewDatabaseSample/Program.cs
+++ CodeFirstNewDatabaseSample/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model;
+using Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,51 @@ namespace CodeFirstNewDatabaseSample
     {
         static void Main(string[] args)
         {
+            using (var db = new BloggingContext())
+            {
+
+
+                // Create and save new Blog
+                Console.WriteLine("Enter a name for a new Blog: ");
+                var newBlogName = Console.ReadLine();
+
+                var newBlog = new Blog { Name = newBlogName };
+                db.Blogs.Add(newBlog);
+                db.SaveChanges();
+
+                // Display all Blogs from the database
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                Console.WriteLine("All Blogs in the database: ");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                // Create and save new User
+                Console.WriteLine("Enter a name for a new User: ");
+                var newUsername = Console.ReadLine();
+
+                var newUser = new User { Username = newUsername };
+                db.Users.Add(newUser);
+                db.SaveChanges();
+
+                // Display all Users from the database
+                var users = from b in db.Users
+                            orderby b.Username
+                            select b;
+
+                Console.WriteLine("All Users in the database: ");
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Username);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
         }
     }
 }
---
+++ Repository/BloggingContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Model;
+
+namespace Repository
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.DisplayName)
+                .HasColumnName("display_name");
+        }
+    } 
+}
